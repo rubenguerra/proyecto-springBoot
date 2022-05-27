50d00afd7944a31a8cac8a29aa0fa3268ba06618
rubenguerra <chopiterguerra@gmail.com>
2022-05-27 11:56:02 -0300

    Agregado @CrossOrigin

--- src/main/java/com/ejemplo/proyecto/controladores/CurriculumController.java
+++ src/main/java/com/ejemplo/proyecto/controladores/CurriculumController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/apiDatos/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CurriculumController {
 
     @Autowired
--- src/main/java/com/ejemplo/proyecto/controladores/UsuarioController.java
+++ src/main/java/com/ejemplo/proyecto/controladores/UsuarioController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/apiUsuario/")
+@CrossOrigin(origins = "http://localhost:4200/usuarios/users/")
 public class UsuarioController {
 
     @Autowired
